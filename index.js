0d521864e72471bf56821d9ddf360db8e8042b75
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-20 02:53:01 +0500

    Calculator

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let userInput = await inquirer.prompt([
+    {
+        message: "Enter first number", //message
+        type: "number", // type kia lai gy user sy
+        name: "firstNumber" //ks name sy save jr ga message inquirer apny pas
+    }, // first message
+    {
+        message: "Enter second number", //message
+        type: "number", // type kia lai gy user sy
+        name: "secondNumber" //
+    }, // second message
+    {
+        message: "Select  one of the operator to perform operation", //message
+        type: "list", // type list dikhay ga
+        name: "operator", //,
+        choices: ["addition", "subtraction", "multiplication", "division"] //,
+    }, // Third message
+]);
+if (userInput.operator === "addition") {
+    console.log(`Your answer is ${userInput.firstNumber + userInput.secondNumber}`);
+}
+else if (userInput.operator === "subtraction") {
+    console.log(`Your answer is ${userInput.firstNumber - userInput.secondNumber}`);
+}
+else if (userInput.operator === "multiplication") {
+    console.log(`Your answer is ${userInput.firstNumber * userInput.secondNumber}`);
+}
+else if (userInput.operator === "division") {
+    console.log(`Your answer is ${userInput.firstNumber / userInput.secondNumber}`);
+}
+else {
+    console.log("Please select valid operator");
+}
